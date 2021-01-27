5bc66a4a55d430dead2fd120f1818c904715f771
vinisklin <vinisklin@gmail.com>
2021-01-26 21:52:29 -0300

    Created main page and Widget component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,45 @@
 import styled from 'styled-components'
+import db from "../db.json";
+import Widget from "../src/components/Widget";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const BackgroundImage = styled.div`
+  background: url(${db.bg}) center/cover;
+  flex: 1;
+`;
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Teste</h1>
+          </Widget.Header>
+          
+          <Widget.Content>
+            <p>Teste doio</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Teste</h1>
+
+            <p>Teste doio</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </BackgroundImage>
+  )
 }
